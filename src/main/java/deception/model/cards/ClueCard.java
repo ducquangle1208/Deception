51887f34af0718d62b302aedda8e692512df5263
ducquangle1208 <ducquangle620@gmail.com>
2026-02-25 16:34:35 +0700

    refactoring

--- src/main/java/deception/model/cards/ClueCard.java
+++ src/main/java/deception/model/cards/ClueCard.java
@@ -8,9 +8,6 @@ import deception.model.abstract_model.Card;
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class ClueCard extends Card {
-
-    private String clue;
-
     @Override
     public CardType getType() {
         return CardType.CLUE;
--- src/main/java/deception/model/cards/MeansCard.java
+++ src/main/java/deception/model/cards/MeansCard.java
@@ -8,8 +8,6 @@ import deception.model.abstract_model.Card;
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class MeansCard extends Card {
-
-    private String weapon;
     @Override
     public CardType getType() {
         return CardType.MEANS;
